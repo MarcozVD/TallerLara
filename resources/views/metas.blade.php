702031e4cceada331d1288fede54602dfe47fa21
MarcozVD <mvaleradaza@gmail.com>
2026-02-16 11:56:03 -0500

    vista metas creada

---
+++ resources/views/metas.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Metas</title>
+</head>
+<body>
+
+    <h1>Mis Metas</h1>
+
+    <ul>
+        <li>Graduarme como ingeniero</li>
+        <li>Ser desarrollador full stack</li>
+        <li>Trabajar en una empresa de tecnología</li>
+    </ul>
+
+    <br>
+    <a href="{{ route('perfil') }}">⬅ Volver al perfil</a>
+
+</body>
+</html>
