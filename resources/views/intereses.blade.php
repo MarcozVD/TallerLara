38e5079884f52056ae37d605715dea7a8b5d9a49
MarcozVD <mvaleradaza@gmail.com>
2026-02-16 11:59:14 -0500

    vista intereses creada

---
+++ resources/views/intereses.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Intereses</title>
+</head>
+<body>
+
+    <h1>Mis Intereses</h1>
+
+    <ul>
+        <li>Programación</li>
+        <li>Desarrollo web</li>
+        <li>Videojuegos</li>
+        <li>Tecnología</li>
+        <li>Ciberseguridad</li>
+    </ul>
+
+    <br>
+    <a href="{{ route('perfil') }}">⬅ Volver al perfil</a>
+
+</body>
+</html>
