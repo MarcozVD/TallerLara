bf3bca43222ce2e511cbe4481969c97caf42615b
MarcozVD <mvaleradaza@gmail.com>
2026-02-16 11:58:01 -0500

    vista habilidades creada

---
+++ resources/views/habilidades.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Habilidades</title>
+</head>
+<body>
+
+    <h1>Mis Habilidades</h1>
+
+    <ul>
+        <li>Docker</li>
+        <li>Node</li>
+        <li>MongoDB</li>
+        <li>Mysql</li>
+        <li>PHP</li>
+        <li>Laravel</li>
+        <li>HTML y CSS</li>
+        <li>Python</li>
+        <li>JavaScript</li>
+    </ul>
+
+    <br>
+    <a href="{{ route('perfil') }}">⬅ Volver al perfil</a>
+
+</body>
+</html>
